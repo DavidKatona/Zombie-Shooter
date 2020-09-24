781cfde5fa0693beec2931031fc4855ee0ce2800
dave94punk <david.katona@hotmail.com>
2020-09-24 11:38:55 +0200

    Added CursorLocker.

---
+++ Assets/Scripts/CharacterController/CursorLocker.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CharacterController
+{
+    public class CursorLocker : MonoBehaviour
+    {
+        #region FIELDS
+
+        private bool _isCursorLocked = true;
+        private bool _shouldLockCursor = true;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public bool IsCursorLocked
+        {
+            get { return _isCursorLocked; }
+            set { _isCursorLocked = value; }
+        }
+
+        public bool ShouldLockCursor
+        {
+            get { return _shouldLockCursor; }
+            set { _shouldLockCursor = value; }
+        }
+
+        #endregion
+
+        #region METHODS
+
+        public void SetCursorLock(bool value)
+        {
+            ShouldLockCursor = value;
+            if (!ShouldLockCursor)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+        private void UpdateCursorLock()
+        {
+            if (ShouldLockCursor)
+            {
+                InternalLockUpdate();
+            }
+        }
+
+        private void InternalLockUpdate()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                IsCursorLocked = false;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                IsCursorLocked = true;
+            }
+
+            if (IsCursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+        #endregion
+
+        #region MONOBEHAVIOUR
+
+        private void Update()
+        {
+            UpdateCursorLock();
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/CharacterController/FPController.cs
+++ Assets/Scripts/CharacterController/FPController.cs
@@ -1,181 +1,194 @@
 ﻿using UnityEngine;
 
-public class FPController : MonoBehaviour
+namespace Assets.Scripts.CharacterController
 {
-    #region EDITOR EXPOSED FIELDS
-
-    [Header("Movement")]
-    [SerializeField] private float _speed = 0.1f;
-
-    [Header("Camera Movement")]
-    [SerializeField] private float _mouseSensitivity = 5f;
-    [SerializeField] private float _maximumCameraPitch = 90f;
-    [SerializeField] private float _minimumCameraPitch = -90f;
-
-    #endregion
-
-    #region FIELDS
-
-    private Quaternion _cameraRotation;
-    private Quaternion _characterRotation;
-
-    #endregion
-
-
-    #region PROPERTIES
-
-    /// <summary>
-    /// Maximum movement speed (in m/s).
-    /// </summary>
-
-    public float Speed
+    public class FPController : MonoBehaviour
     {
-        get { return _speed; }
-        set { _speed = Mathf.Max(0.0f, value); }
-    }
+        #region EDITOR EXPOSED FIELDS
 
-    /// <summary>
-    /// Mouse sensitivity modifier that affects both X and Y axis movement.
-    /// </summary>
+        [Header("Movement")]
+        [SerializeField] private float _speed = 0.1f;
 
-    public float MouseSensitivity
-    {
-        get { return _mouseSensitivity; }
-        set { _mouseSensitivity = value; }
-    }
+        [Header("Camera Movement")]
+        [SerializeField] private float _mouseSensitivity = 5f;
+        [SerializeField] private float _maximumCameraPitch = 90f;
+        [SerializeField] private float _minimumCameraPitch = -90f;
 
-    public float MaximumCameraPitch
-    {
-        get { return _maximumCameraPitch; }
-        set { _maximumCameraPitch = value; }
-    }
+        #endregion
 
-    public float MinimumCameraPitch
-    {
-        get { return _minimumCameraPitch; }
-        set { _minimumCameraPitch = value; }
-    }
+        #region FIELDS
 
-    public Quaternion CameraRotation
-    {
-        get { return _cameraRotation; }
-        set { _cameraRotation = value; }
-    }
+        private float _xAxisInputModifier;
+        private float _zAxisInputModifier;
+        private bool _isJumpPressed;
+        private Quaternion _cameraRotation;
+        private Quaternion _characterRotation;
 
-    public Quaternion CharacterRotation
-    {
-        get { return _characterRotation; }
-        set { _characterRotation = value; }
-    }
+        #endregion
 
-    /// <summary>
-    /// Cached transform component attached to the GameObject.
-    /// </summary>
+        #region PROPERTIES
 
-    public Transform cachedTransform { get; private set; }
+        /// <summary>
+        /// Maximum movement speed (in m/s).
+        /// </summary>
 
-    /// <summary>
-    /// Cached capsule collider component attached to the GameObject.
-    /// </summary>
-    /// 
-    public CapsuleCollider cachedCapsuleCollider { get; private set; }
-
-    /// <summary>
-    /// Cached rigidbody compoonent attached to the GameObject.
-    /// </summary>
-
-    public Rigidbody cachedRigidbody { get; private set; }
-
-    /// <summary>
-    /// Cached camera component attached to the GameObject or one of its children.
-    /// </summary>
-
-    public Camera cachedCamera { get; private set; }
-
-    #endregion
-
-    #region METHODS
-
-    private bool IsGrounded()
-    {
-        RaycastHit hitInfo;
-        if (Physics.SphereCast(cachedTransform.position, cachedCapsuleCollider.radius, Vector3.down, out hitInfo,
-            (cachedCapsuleCollider.height / 2f) - cachedCapsuleCollider.radius + 0.1f))
+        public float Speed
         {
-            return true;
+            get { return _speed; }
+            set { _speed = Mathf.Max(0.0f, value); }
         }
 
-        return false;
-    }
+        /// <summary>
+        /// Mouse sensitivity modifier that affects both X and Y axis movement.
+        /// </summary>
 
-    /// <summary>
-    /// Clamps the pitch (x axis rotation) of the camera component so its constrained between two values.
-    /// </summary>
-    /// <param name="currentCameraRotation"></param>
-    /// <returns></returns>
-
-    private Quaternion ClampCameraPitch(Quaternion currentCameraRotation)
-    {
-        currentCameraRotation.x /= currentCameraRotation.w;
-        currentCameraRotation.y /= currentCameraRotation.w;
-        currentCameraRotation.z /= currentCameraRotation.w;
-        currentCameraRotation.w = 1.0f;
-
-        float cameraPitch = 2.0f * Mathf.Rad2Deg * Mathf.Atan(currentCameraRotation.x);
-        cameraPitch = Mathf.Clamp(cameraPitch, MinimumCameraPitch, MaximumCameraPitch);
-        currentCameraRotation.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * cameraPitch);
-
-        return currentCameraRotation;
-    }
-
-    #endregion
-
-    #region MONOBEHAVIOUR
-
-    private void Awake()
-    {
-        // Cache components.
-
-        cachedTransform = GetComponent<Transform>();
-        cachedCapsuleCollider = GetComponent<CapsuleCollider>();
-        cachedRigidbody = GetComponent<Rigidbody>();
-        cachedCamera = GetComponentInChildren<Camera>();
-
-        // Initialize fields/properties.
-
-        CameraRotation = cachedCamera.transform.localRotation;
-        CharacterRotation = cachedTransform.localRotation;
-    }
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        public float MouseSensitivity
         {
-            cachedRigidbody.AddForce(0, 300, 0);
+            get { return _mouseSensitivity; }
+            set { _mouseSensitivity = value; }
         }
+
+        public float MaximumCameraPitch
+        {
+            get { return _maximumCameraPitch; }
+            set { _maximumCameraPitch = value; }
+        }
+
+        public float MinimumCameraPitch
+        {
+            get { return _minimumCameraPitch; }
+            set { _minimumCameraPitch = value; }
+        }
+
+        public Quaternion CameraRotation
+        {
+            get { return _cameraRotation; }
+            set { _cameraRotation = value; }
+        }
+
+        public Quaternion CharacterRotation
+        {
+            get { return _characterRotation; }
+            set { _characterRotation = value; }
+        }
+
+        /// <summary>
+        /// Cached transform component attached to the GameObject.
+        /// </summary>
+
+        public Transform cachedTransform { get; private set; }
+
+        /// <summary>
+        /// Cached capsule collider component attached to the GameObject.
+        /// </summary>
+        /// 
+        public CapsuleCollider cachedCapsuleCollider { get; private set; }
+
+        /// <summary>
+        /// Cached rigidbody compoonent attached to the GameObject.
+        /// </summary>
+
+        public Rigidbody cachedRigidbody { get; private set; }
+
+        /// <summary>
+        /// Cached camera component attached to the GameObject or one of its children.
+        /// </summary>
+
+        public Camera cachedCamera { get; private set; }
+
+        #endregion
+
+        #region METHODS
+
+        private bool IsGrounded()
+        {
+            RaycastHit hitInfo;
+            if (Physics.SphereCast(cachedTransform.position, cachedCapsuleCollider.radius, Vector3.down, out hitInfo,
+                (cachedCapsuleCollider.height / 2f) - cachedCapsuleCollider.radius + 0.1f))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Clamps the pitch (x axis rotation) of the camera component so its constrained between two values.
+        /// </summary>
+        /// <param name="currentCameraRotation"></param>
+        /// <returns></returns>
+
+        private Quaternion ClampCameraPitch(Quaternion currentCameraRotation)
+        {
+            currentCameraRotation.x /= currentCameraRotation.w;
+            currentCameraRotation.y /= currentCameraRotation.w;
+            currentCameraRotation.z /= currentCameraRotation.w;
+            currentCameraRotation.w = 1.0f;
+
+            float cameraPitch = 2.0f * Mathf.Rad2Deg * Mathf.Atan(currentCameraRotation.x);
+            cameraPitch = Mathf.Clamp(cameraPitch, MinimumCameraPitch, MaximumCameraPitch);
+            currentCameraRotation.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * cameraPitch);
+
+            return currentCameraRotation;
+        }
+
+        #endregion
+
+        #region MONOBEHAVIOUR
+
+        private void Awake()
+        {
+            // Cache components.
+
+            cachedTransform = GetComponent<Transform>();
+            cachedCapsuleCollider = GetComponent<CapsuleCollider>();
+            cachedRigidbody = GetComponent<Rigidbody>();
+            cachedCamera = GetComponentInChildren<Camera>();
+
+            // Initialize fields/properties.
+
+            CameraRotation = cachedCamera.transform.localRotation;
+            CharacterRotation = cachedTransform.localRotation;
+        }
+
+        private void Update()
+        {
+            // Handle input. Should go to its own class.
+
+            if (Input.GetButtonDown("Jump") && IsGrounded())
+            {
+                _isJumpPressed = true;
+            }
+
+            _xAxisInputModifier = Input.GetAxis("Horizontal");
+            _zAxisInputModifier = Input.GetAxis("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            if (_isJumpPressed)
+            {
+                cachedRigidbody.AddForce(0, 300, 0);
+                _isJumpPressed = false;
+            }
+
+            cachedTransform.position += (cachedTransform.forward * _zAxisInputModifier * Speed + cachedTransform.right * _xAxisInputModifier * Speed);
+        }
+
+        private void LateUpdate()
+        {
+            float yRotation = Input.GetAxis("Mouse X") * MouseSensitivity;
+            float xRotation = Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+            CameraRotation *= Quaternion.Euler(-xRotation, 0, 0);
+            CharacterRotation *= Quaternion.Euler(0, yRotation, 0);
+
+            CameraRotation = ClampCameraPitch(CameraRotation);
+
+            cachedCamera.transform.localRotation = CameraRotation;
+            cachedTransform.localRotation = CharacterRotation;
+        }
+
+        #endregion
     }
-
-    private void FixedUpdate()
-    {
-        float x = Input.GetAxis("Horizontal") * Speed;
-        float z = Input.GetAxis("Vertical") * Speed;
-
-        cachedTransform.position += (cachedTransform.forward * z + cachedTransform.right * x);
-    }
-
-    private void LateUpdate()
-    {
-        float yRotation = Input.GetAxis("Mouse X") * MouseSensitivity;
-        float xRotation = Input.GetAxis("Mouse Y") * MouseSensitivity;
-
-        CameraRotation *= Quaternion.Euler(-xRotation, 0, 0);
-        CharacterRotation *= Quaternion.Euler(0, yRotation, 0);
-
-        CameraRotation = ClampCameraPitch(CameraRotation);
-
-        cachedCamera.transform.localRotation = CameraRotation;
-        cachedTransform.localRotation = CharacterRotation;
-    }
-
-    #endregion
 }
