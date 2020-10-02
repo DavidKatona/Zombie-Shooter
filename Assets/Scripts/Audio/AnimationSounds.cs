c4eca12ae799a8638b86eccf33b167637f718b97
dave94punk <david.katona@hotmail.com>
2020-10-02 13:40:46 +0200

    After importing ECM

---
+++ Assets/Scripts/Audio/AnimationSounds.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Extensions;
+using UnityEngine;
+
+public class AnimationSounds : MonoBehaviour
+{
+    #region EDITOR EXPOSED FIELDS
+
+    [Header("Audio Files")]
+    [SerializeField] private AudioClip[] _footstepClips;
+
+    #endregion
+
+    #region PROPERTIES
+
+    public AudioClip[] FootstepClips { get { return _footstepClips; } }
+
+    #endregion
+
+    #region METHODS
+
+    private void PlayFootStepSound()
+    {
+        AudioSource audioSource = new AudioSource();
+        audioSource.InstantiateAudioSource(GetRandomClip(FootstepClips), transform.position);
+    }
+
+    private AudioClip GetRandomClip(AudioClip[] audioClips)
+    {
+        var randomIndex = Random.Range(0, audioClips.Length);
+        return audioClips[randomIndex];
+    }
+
+    #endregion
+}
