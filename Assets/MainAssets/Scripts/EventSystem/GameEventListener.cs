3cb7a83a64583f765a1a187f8f943e13d0d6fdf3
dave94punk <david.katona@hotmail.com>
2020-10-16 13:20:12 +0200

    Reorganized project.

---
+++ Assets/MainAssets/Scripts/EventSystem/GameEventListener.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    #region EDITOR EXPOSED FIELDS
+
+    [SerializeField] private GameEvent _event = null;
+    [SerializeField] private UnityEvent _response = null;
+
+    #endregion
+
+    #region PROPERTIES
+
+    public GameEvent Event { get { return _event; } }
+
+    public UnityEvent Response { get { return _response; } }
+
+    #endregion
+
+    #region METHODS
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+
+    #endregion
+
+    #region MONOBEHAVIOUR
+
+    private void OnEnable()
+    {
+        Event.SubscribeListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnsubscribeListener(this);
+    }
+
+    #endregion
+}
---
+++ Assets/MainAssets/Scripts/ScriptableObjects/GameEvent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Game Event", menuName = "Event Systems/Game Event", order = 0)]
+    public class GameEvent : ScriptableObject
+    {
+        #region FIELDS
+
+        private List<GameEventListener> _listeners = new List<GameEventListener>();
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// The list of listeners subscribed to this game event.
+        /// </summary>
+
+        public List<GameEventListener> Listeners { get { return _listeners; } }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// Raise the event and notify all subscribed listeners.
+        /// </summary>
+
+        public void Raise()
+        {
+            for (int i = Listeners.Count - 1; i >= 0; i--)
+            {
+                Listeners[i].OnEventRaised();
+            }
+        }
+
+        /// <summary>
+        /// Subscribe a new listener to this game event.
+        /// </summary>
+        /// <param name="listener"></param>
+
+        public void SubscribeListener(GameEventListener listener)
+        {
+            Listeners.Add(listener);
+        }
+
+        /// <summary>
+        /// Unsubscribe a listener from this game event.
+        /// </summary>
+        /// <param name="listener"></param>
+
+        public void UnsubscribeListener(GameEventListener listener)
+        {
+            Listeners.Remove(listener);
+        }
+
+        #endregion
+    }
+}
