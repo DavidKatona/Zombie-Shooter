8e26d9679de3fb9a644f11b58880efbaf74823a3
dave94punk <david.katona@hotmail.com>
2020-10-26 13:16:11 +0100

    Implemented a compass bar that points towards the objective.

---
+++ Assets/MainAssets/Scripts/UI/PlayerHUD/Compass/CompassController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.MainAssets.Scripts.UI.PlayerHUD.Compass
+{
+    public class CompassController : MonoBehaviour
+    {
+        #region EDITOR EXPOSED FIELDS
+
+        [SerializeField] private Transform _cachedGoalTransform = null;
+        [SerializeField] private GameObject _pointer = null;
+        [SerializeField] private RectTransform _compassBar = null;
+
+        #endregion
+
+        #region FIELDS
+
+        private Vector3[] _cornerPositions = new Vector3[4];
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// The cached transform component attached to the player.
+        /// </summary>
+
+        public Transform CachedPlayerTransform { get; private set; }
+        
+        /// <summary>
+        /// The cached transform component attached to the goal game object.
+        /// </summary>
+
+        public Transform CachedGoalTransform { get { return _cachedGoalTransform; } }
+
+        /// <summary>
+        /// The UI element (usually an arrow) that points towards the goal's transform.
+        /// </summary>
+
+        public GameObject Pointer { get { return _pointer; } }
+
+        /// <summary>
+        /// The rect transform component of the Pointer.
+        /// </summary>
+
+        public RectTransform PointerRectangle { get; private set; }
+
+        /// <summary>
+        /// The rect transform component of the compass bar game object.
+        /// </summary>
+
+        public RectTransform CompassBar { get { return _compassBar; } }
+
+        /// <summary>
+        /// The scale of the compass bar calculated from the two bottom corners of the compass' rect transform.
+        /// </summary>
+
+        public float CompassBarScale { get; private set; }
+
+        #endregion
+
+        #region MONOBEHAVIOUR
+
+        private void Awake()
+        {
+            // Cache and initialize components.
+
+            CachedPlayerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            PointerRectangle = Pointer.GetComponent<RectTransform>();
+            CompassBar.GetLocalCorners(_cornerPositions);
+            CompassBarScale = Vector3.Distance(_cornerPositions[1], _cornerPositions[2]);
+        }
+
+        private void Update()
+        {
+            // Compute the direction the pointer should aim towards.
+
+            Vector3 direction = CachedGoalTransform.position - CachedPlayerTransform.position;
+
+            // Determine the angle between the player's forward vector and our direction.
+
+            float angleToDirection = Vector3.SignedAngle(CachedPlayerTransform.forward, direction, CachedPlayerTransform.up);
+
+            // Modify the result so it fits the scale of the compass bar, then position it.
+
+            angleToDirection = Mathf.Clamp(angleToDirection, -90, 90) / 180f * CompassBarScale;
+            PointerRectangle.localPosition = new Vector3(angleToDirection, PointerRectangle.localPosition.y, PointerRectangle.localPosition.z);
+        }
+
+        #endregion
+
+    }
+}
